8d965480fe5264261cf45ca6ca7b6654ab53bae3
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-01-09 17:07:06 -0500

    cleaning up js

--- b-script.js
+++ b-script.js
@@ -15,9 +15,31 @@ If I type information into the designated space
 Depending on what time it is
     Hours that passed are color coded differently
     than the current hour and future hours
+
+I have a button
+    When I click the button saveBtn, it saves the input
+    When I resresh the page, the input remains in the textarea
+    The input clears at the end of the day.
 */
 
 $(document).ready(() => {
+        
+    // var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || {};
+    var todaysDate = moment().format("MMMM-DD-YYYY");
+    // console.log(currentHour);
+
+
+    let workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let hourText = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+
+    // Today's date
+    $("#currentDay").text(todaysDate);
+
+    //Append elements to DOM
+    for (let i = 0; i < workHours.length; i++) {
+        $("<div class='row'><div class='hour col-1' id='" + workHours[i] + "'>" + hourText[i] + "</div><textarea name='text' id='input' class='description col-10' placeholder= 'Type something here'></textarea><button class='saveBtn col-1'><i class='far fa-save'></i></button>"
+        ).appendTo(".container");
+    }
 
     var TimeTracker = {
         hour: function () { return moment().hour(); },
@@ -35,95 +57,28 @@ $(document).ready(() => {
                     console.log(element);
                     if (h < hr[i]) {
                         $(element).attr("class", "description col-10 future")
-                        console.log("future: " +
-                            `current hour -> ${h} is less than event hour -> ${hr[i]}
-                            ${element.classList}`); //LOG
+                        // console.log("future: " +
+                        //     `current hour -> ${h} is less than event hour -> ${hr[i]}
+                        //     ${element.classList}`); //LOG
                     } else if (h > hr[i]) {
-                        $(element).attr("class", "description col-10 present")
-                        console.log("past: " +
-                            `current hour -> ${h} is more than event hour -> ${hr[i]}
-                             ${element.classList}`); //LOG
-                    } else {
                         $(element).attr("class", "description col-10 past")
-                        console.log("present: " +
-                            `current hour -> ${h} is equal to event hour -> ${hr[i]}
-                             ${element.classList}`); //LOG
+                        // console.log("past: " +
+                        //     `current hour -> ${h} is more than event hour -> ${hr[i]}
+                        //      ${element.classList}`); //LOG
+                    } else {
+                        $(element).attr("class", "description col-10 present")
+                        // console.log("present: " +
+                        //     `current hour -> ${h} is equal to event hour -> ${hr[i]}
+                        //      ${element.classList}`); //LOG
                     }
                 }
-            }, 5000, hr);
+            }, 1000, hr);
         },
     }
 
-    // var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || {};
-    var todaysDate = moment().format("MMMM-DD-YYYY");
-    // console.log(currentHour);
-
-
-    let workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-    let hourText = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
-
-    // console.log("hour compare: " + moment(workHours[4]).isSame(currentHour));
-    console.log(`time-tracker-hour: ${TimeTracker.hour()}
-                    - for-each begins looping here - `);
     TimeTracker.timecheck(workHours, TimeTracker.hour());
 
-    // let workHours = [
-    //     {time: "9AM", id: "hour1"},
-    //     {time: "10AM", id: "hour2"},
-    //     {time: "11AM", id: "hour3"},
-    //     {time: "12PM", id: "hour4"},
-    //     {time: "1PM", id: "hour5"},
-    //     {time: "2PM", id: "hour6"},
-    //     {time: "3PM", id: "hour7"},
-    //     {time: "4PM", id: "hour8"},
-    //     {time: "5PM", id: "hour9"},
-    // ];
-    //Create id for each hour?
-    // workHours.forEach((item, i) => {
-    //     item.id = i + 1;
-    // })
-    // console.log(workHours);
+    
 
-    // Today's date
-    $("#currentDay").text(todaysDate);
-
-    // const hour = moment.duration(60, "minutes");
-    // console.log(hour);
-
-
-    // let workDay = workHours;
-    let startTime = moment().set({ hour: 9 });
-    let endTime = moment().set({ hour: 5 });
-
-
-    // startOf displays the hour at the top of the hour.
-    // console.log(startTime.startOf("hour"));
-    // console.log(endTime.startOf("hour"));
-
-    //Append elements to DOM
-    for (let i = 0; i < workHours.length; i++) {
-        $("<div class='row'><div class='hour col-1' id='" + workHours[i] + "'>" + hourText[i] + "</div><textarea class='description col-10' placeholder= 'Type something here'></textarea><button class='saveBtn col-1'><i class='far fa-save'></i></button>"
-        ).appendTo(".container");
-    }
-    // for (let i = 0; i < hourText.length; i++) {
-    //     const timeEl = hourText[i];
-    //     $(".hour").html(hourText[i]);
-    // }
-
-    // for (var i = 0; i < workHours.length; i++) {
-    //     $("<div class='row'></div>").appendTo(".container");
-    // }
-    //     // $("div.hour").html(workHours[i].time);
-    //     $("<div class='hour col-1'></div>").appendTo(".row");
-    //     $("<textarea class='description col-10' placeholder= 'Type something here'></textarea>").appendTo(".row");
-    //     $("<button class='saveBtn col-1'>Save</button>").appendTo(".row");
-
-    //  console.log(JSON.stringify(hours));
-    //  moment(60, "m").fromNow();
-
-})
-
-
-
-
-
+   
+});
